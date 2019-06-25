327da469cf78289d305595cfbc0702ef5fcfc3d2
peterpatjeff <pet13051@byui.edu>
2019-06-24 23:19:51 -0600

    fix

--- assignments/lesson-9/js/hamburger.js
+++
@@ -1,3 +0,0 @@
-function toggleNavMenu() {
-    document.getElementById("menu").classList.toggle( "hide" );
-}
--- assignments/lesson-9/js/windchill.js
+++
@@ -1,5 +0,0 @@
-var t = 78; //  high + low / 2 )
-var s = 5; // Wind Speed miles per hour
-var f;
-        f = 35.74 + 0.6215 * t - 35.75 * Math.pow( s, 0.16 ) + 0.4275 * t * Math.pow( s, 0.16 );
-        document.getElementById("windChill").innerHTML = f.toFixed( 2 );
