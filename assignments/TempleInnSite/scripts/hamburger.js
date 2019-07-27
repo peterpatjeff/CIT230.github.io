80b47ffa5dd9f18a731fe440bb5d69e1c6ccd587
peterpatjeff <pet13051@byui.edu>
2019-07-26 19:10:16 -0600

    fix

---
+++ assignments/TempleInnSite/scripts/hamburger.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Hamburger Menu Functionality
+ * 
+ * Ruth Hansen
+ * Date: 4/3/2019  
+ * 
+ * 
+ * 
+ */
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
